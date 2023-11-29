44a28f2802f23ac2a06456607dd268fda1b49d75
ilusin_o@itstep.academy <ilusin_o@itstep.academy>
2023-11-29 16:18:59 +0200

    Notifications

---
+++ 16_Notifications/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace _16_Notifications
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.startBtn = new System.Windows.Forms.Button();
+            this.stopBtn = new System.Windows.Forms.Button();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Black", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(409, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(538, 159);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Counter";
+            // 
+            // startBtn
+            // 
+            this.startBtn.Location = new System.Drawing.Point(307, 339);
+            this.startBtn.Name = "startBtn";
+            this.startBtn.Size = new System.Drawing.Size(270, 131);
+            this.startBtn.TabIndex = 1;
+            this.startBtn.Text = "Start timer";
+            this.startBtn.UseVisualStyleBackColor = true;
+            this.startBtn.Click += new System.EventHandler(this.startBtn_Click);
+            // 
+            // stopBtn
+            // 
+            this.stopBtn.Location = new System.Drawing.Point(798, 339);
+            this.stopBtn.Name = "stopBtn";
+            this.stopBtn.Size = new System.Drawing.Size(265, 131);
+            this.stopBtn.TabIndex = 2;
+            this.stopBtn.Text = "Stop Timer";
+            this.stopBtn.UseVisualStyleBackColor = true;
+            this.stopBtn.Click += new System.EventHandler(this.stopBtn_Click);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "Timer";
+            this.notifyIcon1.Visible = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(17F, 41F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1385, 618);
+            this.Controls.Add(this.stopBtn);
+            this.Controls.Add(this.startBtn);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private Button startBtn;
+        private Button stopBtn;
+        private System.Windows.Forms.Timer timer;
+        private NotifyIcon notifyIcon1;
+    }
+}
---
+++ 16_Notifications/Form1.cs
@@ -0,0 +1,55 @@
+﻿namespace _16_Notifications
+{
+    public partial class Form1 : Form
+    {
+        int count = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            timer.Interval = 1000;
+            timer.Tick += Timer_Tick;
+
+            this.ShowInTaskbar = false;
+            notifyIcon1.Click += NotifyIcon1_Click;
+        }
+
+        private void NotifyIcon1_Click(object? sender, EventArgs e)
+        {
+            this.ShowInTaskbar = true;
+            this.WindowState = FormWindowState.Maximized;
+        }
+
+        private void Timer_Tick(object? sender, EventArgs e)
+        {
+            label1.Text = (++count).ToString(); 
+        }
+
+        private void startBtn_Click(object sender, EventArgs e)
+        {
+            timer.Start();
+            startBtn.Enabled = false;
+            stopBtn.Enabled = true;
+            notifyIcon1.BalloonTipIcon = ToolTipIcon.Info;
+            notifyIcon1.BalloonTipText = "Таймер почав відлік!";
+            notifyIcon1.BalloonTipTitle = "Увага!";
+
+            notifyIcon1.ShowBalloonTip(5);
+
+            //notifyIcon1.ShowBalloonTip()
+
+        }
+
+        private void stopBtn_Click(object sender, EventArgs e)
+        {
+            timer.Stop();
+            startBtn.Enabled = true;
+            stopBtn.Enabled = false;
+            notifyIcon1.BalloonTipIcon = ToolTipIcon.Warning;
+            notifyIcon1.BalloonTipText = "Таймер зупинився!";
+            notifyIcon1.BalloonTipTitle = "Увага!";
+
+            notifyIcon1.ShowBalloonTip(5);
+
+        }
+    }
+}
